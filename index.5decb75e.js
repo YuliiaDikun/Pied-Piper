c8b9476a4fdd3b63c412eb607a77364e87105edd
YuliiaDikun <107618913+YuliiaDikun@users.noreply.github.com>
2022-10-11 18:13:08 +0000

    Deploying to gh-pages from @ YuliiaDikun/Pied-Piper@7e72485fd565af581a304d9dc3c1089fe86b18d5 🚀

---
+++ index.5decb75e.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",type:"bullets",clickable:!0,dynamicBullets:!0,dynamicMainBullets:1}});
+//# sourceMappingURL=index.5decb75e.js.map

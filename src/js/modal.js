272e8de29ff66c4bec0b2034bb0cb4f4a77cf8fe
unknown <dikun.yulia@gmail.com>
2022-10-12 12:01:14 +0300

    add code to other modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,12 +2,48 @@
   const refs = {
     openModalFirstBtn: document.querySelector("[data-modal-open]"),
     openModalSecondBtn: document.querySelector("[data-modal-open-second]"),
+    
+    // Read more
+
+    // 
+
+    // Buy now
+
+    //
+    
+    // Ingredients
+
+    //
 
     closeModalFirstBtn: document.querySelector("[data-modal-close]"),
     closeModalSecondBtn: document.querySelector("[data-modal-close-second]"),
+    
+    // Read more
+
+    // 
+
+    // Buy now
+
+    //
+    
+    // Ingredients
+
+    //
 
     modalLocation: document.querySelector("[data-modal-location]"),
     modalFranchise: document.querySelector("[data-modal-franchise]"),
+    
+    // Read more
+
+    //
+    
+    // Buy now
+
+    //
+
+    // Ingredients
+
+    //
   };
 
   function toggleModal(modal) {
@@ -32,6 +68,44 @@
     toggleModal(secondModal);    
   });
 
+                        // Read more //
+  // refs.openModalFirstBtn.addEventListener("click", function() {
+  //   const firstModal = refs.modalLocation;
+  //   toggleModal(firstModal);    
+  // });
+
+  // refs.openModalSecondBtn.addEventListener("click",  function() {
+  //   const secondModal = refs.modalFranchise;
+  //   toggleModal(secondModal);    
+  // });
+  // 
+  
+                        // Buy now //
+  // refs.openModalFirstBtn.addEventListener("click", function() {
+  //   const firstModal = refs.modalLocation;
+  //   toggleModal(firstModal);    
+  // });
+
+  // refs.openModalSecondBtn.addEventListener("click",  function() {
+  //   const secondModal = refs.modalFranchise;
+  //   toggleModal(secondModal);    
+  // });
+  //
+
+  
+                          // Ingredients //
+  // refs.openModalFirstBtn.addEventListener("click", function() {
+  //   const firstModal = refs.modalLocation;
+  //   toggleModal(firstModal);    
+  // });
+
+  // refs.openModalSecondBtn.addEventListener("click",  function() {
+  //   const secondModal = refs.modalFranchise;
+  //   toggleModal(secondModal);    
+  // });
+  //
+  
+  
   // CLOSE init //
 
   refs.closeModalFirstBtn.addEventListener("click", function() {
@@ -42,5 +116,35 @@
     const secondModal = refs.modalLocation;
     close(secondModal);    
   }); 
+
+                          // Read more
+  // refs.closeModalFirstBtn.addEventListener("click", function() {
+  //   const firstModal = refs.modalLocation;
+  //   close(firstModal);    
+  // });
+  // refs.closeModalSecondBtn.addEventListener("click", function() {
+  //   const secondModal = refs.modalLocation;
+  //   close(secondModal);    
+  // }); 
+  
+                          // Buy now
+  // refs.closeModalFirstBtn.addEventListener("click", function() {
+  //   const firstModal = refs.modalLocation;
+  //   close(firstModal);    
+  // });
+  // refs.closeModalSecondBtn.addEventListener("click", function() {
+  //   const secondModal = refs.modalLocation;
+  //   close(secondModal);    
+  // });
+  
+                            // Ingredients
+  // refs.closeModalFirstBtn.addEventListener("click", function() {
+  //   const firstModal = refs.modalLocation;
+  //   close(firstModal);    
+  // });
+  // refs.closeModalSecondBtn.addEventListener("click", function() {
+  //   const secondModal = refs.modalLocation;
+  //   close(secondModal);    
+  // });
 
 })();
